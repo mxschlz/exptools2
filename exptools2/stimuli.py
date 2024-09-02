8009f91bec301479c63d2a79f27b343a6dde3055
Max <schulz.max5@gmail.com>
2024-09-02 16:19:34 +0200

    Added cross fixation cross

--- exptools2/stimuli.py
+++ exptools2/stimuli.py
@@ -1,7 +1,11 @@
-from psychopy.visual import Circle
+from psychopy.visual import Circle, TextStim
 
 
 def create_circle_fixation(win, radius=0.1, color=(1, 1, 1),
                            edges=100, **kwargs):
     """ Creates a circle fixation dot with sensible defaults. """
     return Circle(win, radius=radius, color=color, edges=edges, **kwargs)
+
+
+def create_fixation_cross(win, text="+", color=(1, 1, 1), height=0.1, pos=(0, 0), **kwargs):
+    return TextStim(win, pos=pos, color=color, height=height, text=text, **kwargs)
