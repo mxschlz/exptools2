9466fcb9c8b7e47c046d94571609480046ff2a16
Max <schulz.max5@gmail.com>
2025-05-13 13:11:19 +0200

    Refactor `setup.py` to enhance structure and maintainability
    Reorganized `setup.py` by modularizing metadata and logic. Introduced dynamic version construction, refined package information, and supported console script entry points. This improves readability and aligns with best practices for package setup.

--- setup.py
+++ setup.py
@@ -1,40 +1,91 @@
-import setuptools
+import os.path as op
+import os
+from setuptools import setup, find_packages
 
-# Read the contents of your README file
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+_version_major = 0
+_version_minor = 1
+_version_micro = ''  # use '' for first of series, number for 1 and above
+_version_extra = 'dev'
+# _version_extra = ''  # Uncomment this for full releases
 
-setuptools.setup(
-    name="exptools2",  # This is the name that will be used for pip install
-    version="0.1.0",  # version number
-    author="Max Schulz",
-    author_email="schulz.max5@gmail.com",
-    description="A Python-based platform for building trial-based experiments with PsychoPy", # A short description
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/mxschlz/exptools2",  # Replace with your GitHub repo URL
-    packages=setuptools.find_packages(), # Automatically find all packages
-    # OR explicitly list them if find_packages() is too broad or you have a flat structure
+# Construct full version string from these.
+_ver = [_version_major, _version_minor]
+if _version_micro:
+    _ver.append(_version_micro)
+if _version_extra:
+    _ver.append(_version_extra)
 
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License", # Choose your license
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha", # Or Beta, Production/Stable
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-    ],
-    python_requires='>=3.10',  # Specify compatible Python versions
-    install_requires=[
-        'psychopy>=3.0.4',
-        'pyyaml',
-        'pandas>=0.23.0',
-        'numpy>=1.14.3',
-        'msgpack_numpy'
-    ],
-    # If you have data files that need to be included with your package:
-    # package_data={
-    #     'exptools2': ['data/*.csv', 'templates/*.html'],
-    # },
-    # include_package_data=True, # if you use MANIFEST.in for more complex data inclusion
+__version__ = '.'.join(map(str, _ver))
+
+CLASSIFIERS = ["Development Status :: 3 - Alpha",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: MIT License",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python",
+               "Topic :: Scientific/Engineering"]
+
+# Description should be a one-liner:
+description = "exptools2: stimulus presentation for psychophysics experiments"
+# Long description will go up on the pypi page
+long_description = """
+exptools2
+=========
+Bla bla.
+To get started, please go to the repository README_.
+.. _README: https://github.com/VU-Cog-Sci/exptools2/blob/master/README.md
+"""
+
+NAME = "exptools2"
+MAINTAINER = "Tomas Knapen"
+MAINTAINER_EMAIL = "tomasknapen@gmail.com"
+DESCRIPTION = description
+LONG_DESCRIPTION = long_description
+URL = "http://github.com/VU-Cog-Sci/exptools2"
+DOWNLOAD_URL = ""
+LICENSE = "MIT"
+AUTHOR = "Tomas Knapen"
+AUTHOR_EMAIL = "tomasknapen@gmail.com"
+PLATFORMS = "OS Independent"
+MAJOR = _version_major
+MINOR = _version_minor
+MICRO = _version_micro
+VERSION = __version__
+PACKAGE_DATA = {'exptools2': [op.join('data', '*')]}
+PACKAGES = find_packages()
+REQUIRES = [
+    'psychopy>=3.0.4',
+    'pyyaml',
+    'pandas>=0.23.0',
+    'numpy>=1.14.3',
+    'msgpack_numpy'
+]
+
+opts = dict(
+    name=NAME,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    platforms=PLATFORMS,
+    version=VERSION,
+    packages=PACKAGES,
+    package_data=PACKAGE_DATA,
+    install_requires=REQUIRES,
+    entry_points={
+                'console_scripts': [
+                    'exptools2_fLoc = exptools2.experiments.fLoc.main:main_api',
+                    ]
+                }
 )
+
+
+if __name__ == '__main__':
+    setup(**opts)
